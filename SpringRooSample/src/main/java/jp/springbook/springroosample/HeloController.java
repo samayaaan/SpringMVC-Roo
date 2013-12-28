f3629faceee9774cf37f2daea710dc13c9eab9bf
ito masaya <itomasaya@team-lab.com>
2013-12-29 01:19:03 +0900

    自動生成したcontrollerとjspにformを追加 Signed-off-by: ito masaya <itomasaya@team-lab.com>

--- SpringRooSample/src/main/java/jp/springbook/springroosample/HeloController.java
+++ SpringRooSample/src/main/java/jp/springbook/springroosample/HeloController.java
@@ -1,8 +1,12 @@
 package jp.springbook.springroosample;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,10 +17,20 @@ public class HeloController {
 
     @RequestMapping(method = RequestMethod.POST, value = "{id}")
     public void post(@PathVariable Long id, ModelMap modelMap, HttpServletRequest request, HttpServletResponse response) {
+    	// ただのサンプル？の模様
     }
 
     @RequestMapping
-    public String index() {
+    public String index(Model model) {
+    	model.addAttribute("formData", new HeloFormData());
         return "helo/index";
     }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/form")
+    public String post(@ModelAttribute HeloFormData form, Errors resut, Model model){
+    	model.addAttribute("message", "You typed: " + form.getInput());
+    	model.addAttribute("formData", new HeloFormData());
+    	return "helo/index";
+    }
+    
 }
---
+++ SpringRooSample/src/main/java/jp/springbook/springroosample/HeloFormData.java
@@ -0,0 +1,13 @@
+package jp.springbook.springroosample;
+
+public class HeloFormData {
+	private String input;
+
+	public String getInput() {
+		return input;
+	}
+
+	public void setInput(String input) {
+		this.input = input;
+	}
+}
